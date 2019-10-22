2e485d1662f6a272f3cac0d3c5ba87da5ef55c63
Hendi Wibowo <hendi.wibowo@usadi.id>
2019-10-22 15:06:19 +0700

    Additional Resources

---
+++ Resources/ConstantTable.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Collections;
+using System.Collections.Generic;
+using Ext.Net;
+using Ext.Net.Utilities;
+using CoreNET.Common.Base;
+using CoreNET.Common.BO;
+
+namespace CoreNET.Common.BO
+{
+  #region ConstantTables
+  [Serializable]
+  public class ConstantTablesDM
+  {
+    
+
+  }
+  #endregion ConstantTables
+}
