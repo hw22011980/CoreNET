49565a37d268a3674e91371250c67935843915e4
Hendi Wibowo <hendi.wibowo@usadi.id>
2019-11-05 00:49:22 +0700

    Folder WebCSM dan WebCMSDebug

---
+++ synchronize_webcms.bat
@@ -0,0 +1,3 @@
+xcopy /s/e/y/k/d "WebCMS" "WebCMSTest" /exclude:synchronize_webcms.txt
+
+set /p temp="Hit enter to continue"
--- syncronize_webcms.bat
+++
@@ -1,3 +0,0 @@
-xcopy /s/e/y/k/d "WebCMS" "WebCMSTest" /exclude:syncronize_webcms.txt
-
-set /p temp="Hit enter to continue"
