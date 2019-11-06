a15b10f7419c433f8b7db77d6f3552589718cb6c
Hendi Wibowo <hendi.wibowo@usadi.id>
2019-11-07 02:03:05 +0700

    Update and restart Launcher

---
+++ Resources/Script/deploy.bat
@@ -0,0 +1,5 @@
+cd "D:\CoreNET\Resources\Script"
+
+copy /y "D:\CoreNET\Project\*\bin\Release" "D:\CoreNET\WebCMS\bin" 
+
+set /p temp="Hit enter to continue"
---
+++ Resources/Script/update_launcher.bat
@@ -0,0 +1,6 @@
+taskkill /IM CoreNETLauncher.exe
+xcopy /s/e/y/k/d "D:\CoreNET\Resources\Script\UpdateLauncher" "D:\CoreNET\Launcher" 
+
+rem set /p temp="Hit enter to continue"
+
+D:\CoreNET\Launcher\CoreNETLauncher.exe
