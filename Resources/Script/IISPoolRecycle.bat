32433deaeaa91c2a4fd0a63e8a9660357efb21dc
Hendi Wibowo <hendi.wibowo@usadi.id>
2019-11-07 15:29:27 +0700

    CoreNETLauncher.exe versi 70

--- Resources/Script/IISPoolRecycle.bat
+++ Resources/Script/IISPoolRecycle.bat
@@ -2,6 +2,9 @@
 cd %WINDIR%\System32\inetsrv
 
 appcmd recycle apppool /apppool.name:"CoreNETPool"
+appcmd start apppool /apppool.name:CoreNETPool
+appcmd start site /site.name:"CoreNETCMS"
+
 
 rem set /p temp="Hit enter to continue"
 
