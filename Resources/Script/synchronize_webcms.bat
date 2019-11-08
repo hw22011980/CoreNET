e0c84f23a7a2b9f3695e583f460db00410cdbf88
Hendi Wibowo <hendi.wibowo@usadi.id>
2019-11-08 18:15:09 +0700

    synchronize_webcms.bat

--- Resources/Script/synchronize_webcms.bat
+++ Resources/Script/synchronize_webcms.bat
@@ -1,4 +1,4 @@
-cd "D:\CoreNET\Resources\Script"
+cd "D:\CoreNET\Launcher\Tool"
 xcopy /s/e/y/k/d "D:\CoreNET\WebCMS" "D:\CoreNET\WebCMSDebug" /exclude:synchronize_webcms.txt
 
 rem set /p temp="Hit enter to continue"
